bff617d69debf07a5a9ccce48ca7ffc8d81d8ccd
Alexander Drakon <drakon.a.v@gmail.com>
2023-11-16 16:34:47 +0300

    fix: cookies tuning

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,7 +12,7 @@ module.exports.cors = (req, res, next) => {
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res,header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', origin);
   }
   if (method === 'OPTIONS') {
